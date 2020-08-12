45773a702b67d10b03ae3c3326055fefff459e94
vishal deswal <vishaldeswal2000@gmail.com>
2020-08-12 22:28:36 +0530

    commited

--- node/test.js
+++ node/test.js
@@ -1,2 +1,3 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
-console.log( say("Node.js") );
+const { solve } = require('../pkg/quadratic_lib.js');
+
+console.log( solve([2., 5., -3.]) );
